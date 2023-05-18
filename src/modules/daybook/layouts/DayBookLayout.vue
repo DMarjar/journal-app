03d8e9ff838bd0aaf2acb5ae3225261315d7e08d
DMarjar <20213tn119@utez.edu.mx>
2023-05-18 15:53:22 -0600

    Estructura de proyecto y rutas basicas
    Se crea la estructura del proyecto basica y se añadieron algunas rutas basicas como cimientos para la aplicacion.

---
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <DaybookNavbar />
+    <div class="d-flex">
+      <div class="col-4">
+        <EntryList />
+      </div>
+      <div class="col">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+
+export default {
+  components: {
+    DaybookNavbar: defineAsyncComponent(() =>
+      import("@/modules/components/DaybookNavbar.vue")
+    ),
+    EntryList: defineAsyncComponent(() =>
+      import("@/modules/daybook/components/EntryList.vue")
+    ),
+  },
+};
+</script>
